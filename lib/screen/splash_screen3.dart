75c8cf504dea36a1c67606aa97cc05e7f7134c38
muhamadfarhannabawi <1123150170@global.ac.id>
2026-01-05 10:19:46 +0700

    terusan sizedbox lagi

--- lib/screen/splash_screen3.dart
+++ lib/screen/splash_screen3.dart
@@ -49,3 +49,12 @@ class SplashScreen3 extends StatelessWidget {
                     color: Colors.grey,
                   ),
                 ),
+                 SizedBox(width: 10),
+                Container(
+                  width: 12,
+                  height: 12,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey,
+                  ),
+                ),
