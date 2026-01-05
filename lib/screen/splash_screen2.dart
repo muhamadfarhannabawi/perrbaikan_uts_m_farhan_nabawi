8d3f5b0e8312eb0ca67c17a5b0896ae44d01fc65
muhamadfarhannabawi <1123150170@global.ac.id>
2026-01-05 10:09:01 +0700

    membuat container

--- lib/screen/splash_screen2.dart
+++ lib/screen/splash_screen2.dart
@@ -14,3 +14,15 @@ class SplashScreen2 extends StatelessWidget {
           children: [
             Spacer(),
             SizedBox(height: 50),
+            Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.deepPurple,
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('android/assets/images/image_4.jpg'),
+                ),
+              ),
+            ),
