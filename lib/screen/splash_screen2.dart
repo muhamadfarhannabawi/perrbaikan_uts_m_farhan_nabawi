05b69a35f12506506f85564161da882ad0edc317
muhamadfarhannabawi <1123150170@global.ac.id>
2026-01-05 10:31:39 +0700

    menambahkan foto untuk screen 2

--- lib/screen/splash_screen2.dart
+++ lib/screen/splash_screen2.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:uts/screen/splash_screen3.dart';
+import 'package:perbaikan_uts_app_mobile/screen/splash_screen3.dart';
 
 class SplashScreen2 extends StatelessWidget {
   const SplashScreen2({super.key});
