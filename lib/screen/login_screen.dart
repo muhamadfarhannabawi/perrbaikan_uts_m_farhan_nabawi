5fa71e331df269202d2cac890e7cd491c803bc0b
muhamadfarhannabawi <1123150170@global.ac.id>
2026-01-05 10:36:36 +0700

    membuat overide

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -2,3 +2,27 @@ import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
+
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(height: 50),
+
+              // 🔹 Logo atau Icon
+              Center(
+                child: Image.asset(
+                  "android/assets/images/image_2.jpg",
+                  width: 100,
+                  height: 230,
+                  fit: BoxFit.cover,
+                ),
+              ),
+
+              const SizedBox(height: 30),
