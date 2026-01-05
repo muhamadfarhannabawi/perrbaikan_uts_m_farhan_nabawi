b945a7f0cf40de63a7245b46f7cd12582fe960e2
muhamadfarhannabawi <1123150170@global.ac.id>
2026-01-05 10:07:40 +0700

    membuat overide

--- lib/screen/splash_screen2.dart
+++ lib/screen/splash_screen2.dart
@@ -3,3 +3,14 @@ import 'package:uts/screen/splash_screen3.dart';
 
 class SplashScreen2 extends StatelessWidget {
   const SplashScreen2({super.key});
+
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+      child:  Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            SizedBox(height: 50),
