7441c5c42069c1609045389ee3881a25fb11c39d
muhamadfarhannabawi <1123150170@global.ac.id>
2026-01-05 10:29:49 +0700

    menambahkan foto

--- lib/screen/splash_screen1.dart
+++ lib/screen/splash_screen1.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:uts/screen/splash_screen2.dart';
+import 'package:perbaikan_uts_app_mobile/screen/splash_screen2.dart';
 
 
 class SplashScreen1 extends StatelessWidget {
