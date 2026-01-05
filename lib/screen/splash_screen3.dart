06b63f499b2221d30485c2f495f2e14feb314f72
muhamadfarhannabawi <1123150170@global.ac.id>
2026-01-05 10:18:22 +0700

    membuat sizedbox

--- lib/screen/splash_screen3.dart
+++ lib/screen/splash_screen3.dart
@@ -26,3 +26,16 @@ class SplashScreen3 extends StatelessWidget {
                 ),
               ),
             ),
+            //tambahkan disini untuk widget widget lainnya nanti
+            SizedBox(height: 30),
+            Text(
+              'Welcome',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Visca Barca Visca Catalunya',
+              style: TextStyle(fontSize: 20, color: Colors.grey[700]),
+            ),
+            SizedBox(height: 50),
+            Row(
